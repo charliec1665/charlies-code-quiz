0c8de40b6645dc49b2bbb5d3047af9ca3db40cb3
Charlie Carter <charliec1665@gmail.com>
2022-08-06 14:30:06 -0400

    pseudocode js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// create conditional to show only one container at a time, using "data-state 'visible'"
+// ceater timer for 75 seconds
+    // when timer ends use conditional to take user to times-up container using data-state
+// create eventlistener for button clicks
+    // when clicking question buttons take to next question
+    // when answering incorrectly, use conditional to subtract 10secs from timer
+    // when clicking initials submit button take to highscores page
+// create function to add initials and question scores to localstorage
+    // access localstorage to display initials and score to highscores page
